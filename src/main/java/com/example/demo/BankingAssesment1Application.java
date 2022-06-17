3e131663e9145f35ce0cd7b24feca4e57e682944
gowshi <gowsigowsalyait923@gmail.com>
2022-06-17 01:52:13 -0700

    First commit

---
+++ src/main/java/com/example/demo/BankingAssesment1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAssesment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAssesment1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/BankingAssesment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAssesment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
